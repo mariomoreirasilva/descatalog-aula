afa667abc9d159c8e2242ffa1e8f0df7d68b724c
mariomoreirasilva <mario@desban.org.br>
2024-05-13 15:19:41 -0300

    Rest, Service. Repository, banco H2 ok

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){	
+		//testando a API
+		/*
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Teste Cat 1"));
+		list.add(new Category(2L, "Teste Cat 2"));
+		return ResponseEntity.ok().body(list);	
+		*/	
+		//fim teste
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+
+}
