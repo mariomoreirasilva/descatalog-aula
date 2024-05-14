d37d715d91807c5485443b666eb33bc5d8542909
mariomoreirasilva <mario@desban.org.br>
2024-05-14 12:55:01 -0300

    FindByid pronto sem tratamento de excessao

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,16 +1,18 @@
 package com.devsuperior.dscatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+
 
 @Service
 public class CategoryService {
@@ -25,10 +27,27 @@ public class CategoryService {
 	}
 	//fim teste	 
 	 */
-	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
+		//expressão lambida
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//semelhante abaixo:
+		/*
+		List<CategoryDTO> listDTO = new ArrayList<>();
+		for(Category cat : list) {
+			listDTO.add(new CategoryDTO(cat));
+		}
+		*/
+		//fim demostração lambida		
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		//optional evita trabalhar com valor nulo
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 		
 	}
 }
